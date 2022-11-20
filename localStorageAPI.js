4a4b27b0d0c7abc6effb79b1cc3ecbbe5ccd3976
Stephen Finch <smfinch99@gmail.com>
2022-11-20 17:23:04 -0500

    add barns

--- localStorageAPI.js
+++ localStorageAPI.js
@@ -8,3 +8,14 @@ function getChickenData(key) {
   const chickenData = JSON.parse(localStorage.getItem('chickens')) || { price: 10, total: 0, bonus: 1 }
   return chickenData[key] ?? chickenData
 }
+
+function setBarnData(key, value) {
+  const newBarnData = getBarnData()
+  newBarnData[key] = value
+  localStorage.setItem('barns', JSON.stringify(newBarnData))
+}
+
+function getBarnData(key) {
+  const barnData = JSON.parse(localStorage.getItem('barns')) || { price: 1000, total: 0, bonus: 25 }
+  return barnData[key] ?? barnData
+}
--- shop.js
+++ shop.js
@@ -1,3 +1,4 @@
+// Chickens
 function buyChicken() {
   if (Number(localStorage.getItem('score')) >= getChickenData('price')) {
     setChickenData('total', getChickenData('total') + 1)
@@ -28,9 +29,42 @@ function updateChickenButton() {
   chickenBuyButtonDisplay.textContent = `Buy (${getChickenData('price')})`
 }
 
+// Barns
+function buyBarn() {
+  if (Number(localStorage.getItem('score')) >= getBarnData('price')) {
+    setBarnData('total', getBarnData('total') + 1)
+    localStorage.setItem('score', Number(localStorage.getItem('score')) - getBarnData('price'))
+    setBarnData('price', Math.round(getBarnData('price') * 1.33))
+    const coinSounds = [
+      'https://cdn.freesound.org/previews/630/630018_12715066-lq.mp3',
+      'https://cdn.freesound.org/previews/630/630018_12715066-lq.mp3',
+      'https://cdn.freesound.org/previews/630/630018_12715066-lq.mp3',
+      'https://cdn.freesound.org/previews/213/213982_3635427-lq.mp3',
+    ]
+    const randomSound = coinSounds[Math.floor(Math.random() * coinSounds.length)]
+
+    playSound(randomSound, 0.5)
+    showScore()
+    updateBarnButton()
+  } else {
+    playSound('https://cdn.freesound.org/previews/429/429720_7872621-lq.mp3')
+    console.log('you do not have enough eggs')
+  }
+}
+
+function updateBarnButton() {
+  const barnTotalDisplay = document.getElementById('barn-total')
+  const barnBuyButtonDisplay = document.getElementById('barn-buy-button')
+
+  barnTotalDisplay.textContent = `${getBarnData('total')} (+${getBarnData('bonus') * getBarnData('total')} eps)`
+  barnBuyButtonDisplay.textContent = `Buy (${getBarnData('price')})`
+}
+
 window.setInterval(() => {
   localStorage.setItem('score', Number(localStorage.getItem('score')) + getChickenData('bonus') * getChickenData('total'))
+  localStorage.setItem('score', Number(localStorage.getItem('score')) + getBarnData('bonus') * getBarnData('total'))
   showScore()
 }, 1000)
 
 updateChickenButton()
+updateBarnButton()
